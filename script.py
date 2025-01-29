8c5cae3322a10c0497bc1f7fcda639958c18f50f
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2025-01-29 13:45:53 -0600

    [Added] script.py file for first class

---
+++ script.py
@@ -0,0 +1,14 @@
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def bark(self):
+        print("Woof woof!")
+
+
+dog1 = Dog()
+dog1.bark()
+
+dog2 = Dog()
+dog2.bark()
